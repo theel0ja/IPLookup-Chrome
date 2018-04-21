ab790f6c2f89e02e313fa23aa4f84a799858b763
Elias Ojala <git@eliasojala.me>
2018-04-22 02:01:58 +0300

    Popup

--- popup.js
+++ popup.js
@@ -1,21 +1,53 @@
-// Copyright 2018 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+// Copyright 2018 Elias Ojala. All rights reserved.
 
 'use strict';
 
-let changeColor = document.getElementById('changeColor');
 
-chrome.storage.sync.get('color', function(data) {
-  changeColor.style.backgroundColor = data.color;
-  changeColor.setAttribute('value', data.color);
+// https://stackoverflow.com/a/23945027/6451184
+function extractHostname(url) {
+  var hostname;
+  //find & remove protocol (http, ftp, etc.) and get hostname
+
+  if (url.indexOf("://") > -1) {
+      hostname = url.split('/')[2];
+  }
+  else {
+      hostname = url.split('/')[0];
+  }
+
+  //find & remove port number
+  hostname = hostname.split(':')[0];
+  //find & remove "?"
+  hostname = hostname.split('?')[0];
+
+  return hostname;
+}
+
+
+// Elements
+let ipAddressElement = document.getElementById('ipAddress');
+let domainElement = document.getElementById('domain');
+let openIPlookupBtn = document.getElementById('openIplookup');
+let debugElement = document.getElementById('debugElement');
+
+// Set ip address
+let ipAddress = undefined;
+ipAddressElement.innerText = ipAddress;
+
+// Set hostname etc
+chrome.tabs.query({
+  active: true,
+  currentWindow: true
+}, function (tabs) {
+  let pageUrl = tabs[0].url;
+  
+  let hostname = extractHostname(pageUrl);
+  domainElement.innerText = hostname;
+
+  openIplookup.href =
+    "https://iplookup.theel0ja.info/chrome?host=" + hostname + "&ip=" + ipAddressElement.innerText + "&v=" + chrome.app.getDetails().version;
+
+  // debugElement.innerHTML = JSON.stringify(ipAddress);
+
+  // ipAddressElement.innerText = ipAddress;
 });
-
-changeColor.onclick = function(element) {
-  let color = element.target.value;
-  chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-    chrome.tabs.executeScript(
-        tabs[0].id,
-        {code: 'document.body.style.backgroundColor = "' + color + '";'});
-  });
-};
