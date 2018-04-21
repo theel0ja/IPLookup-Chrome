444314e160818fabe242eda875e573cdadee70db
Elias Ojala <git@eliasojala.me>
2018-04-22 01:18:08 +0300

    any site

--- background.js
+++ background.js
@@ -10,9 +10,7 @@ chrome.runtime.onInstalled.addListener(function() {
   });
   chrome.declarativeContent.onPageChanged.removeRules(undefined, function() {
     chrome.declarativeContent.onPageChanged.addRules([{
-      conditions: [new chrome.declarativeContent.PageStateMatcher({
-        pageUrl: {hostEquals: 'developer.chrome.com'},
-      })],
+      conditions: [new chrome.declarativeContent.PageStateMatcher()],
       actions: [new chrome.declarativeContent.ShowPageAction()]
     }]);
   });
